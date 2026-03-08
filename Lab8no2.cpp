027641640527cce14ac5caa359fb4c34013a5ac3
Pramika6704062617075 <s6704062617075@email.kmutnb.ac.th>
2026-03-08 14:41:36 +0700

    Commit 3

--- Lab8no2.cpp
+++ Lab8no2.cpp
@@ -3,23 +3,33 @@ int checkscore(char std[]);
 
 int main() {
     int i,j;
-	char s[10] ;
+    int count = 0;   // 
+	char s[10];
+	
     char ans[8][10]={
-        	{'A','B','A','C','C','D','E','E','A','D'},
-			{'D','B','A','B','C','A','E','E','A','D'},
-			{'E','D','D','A','C','B','E','E','A','D'},
-			{'C','B','A','E','D','C','E','E','A','D'},
-			{'A','B','D','C','C','D','E','E','A','D'},
-			{'B','B','E','C','C','D','E','E','A','D'},
-			{'B','B','A','C','C','D','E','E','A','D'},
-			{'E','B','E','C','C','D','E','E','A','D'}} ;
+        {'A','B','A','C','C','D','E','E','A','D'},
+        {'D','B','A','B','C','A','E','E','A','D'},
+        {'E','D','D','A','C','B','E','E','A','D'},
+        {'C','B','A','E','D','C','E','E','A','D'},
+        {'A','B','D','C','C','D','E','E','A','D'},
+        {'B','B','E','C','C','D','E','E','A','D'},
+        {'B','B','A','C','C','D','E','E','A','D'},
+        {'E','B','E','C','C','D','E','E','A','D'}
+    };
+
+    char key1 = 'D';   // 
 
    	for ( i = 0 ; i < 8 ; i++ ){
    		for(j = 0 ; j < 10 ; j++ )
-   		s[j] = ans[i][j] ;                                 
+   			s[j] = ans[i][j];
+
+        if(ans[i][0] == key1)   // 
+            count++;
+
    		printf("std %d => %d\n", (i+1), checkscore(s));
-	   }
-   	
+	}
+
+    printf("Q1 correct is %d \n", count);
 }
 
 int checkscore(char std[]){
@@ -28,13 +38,8 @@ int checkscore(char std[]){
 	for( i = 0 ; i < 10 ; i++ ){
 		if ( std[i] == charkeys[i] ){
 			n++ ;
-			
 		}
-
 	}
-	
 	return n ;
 }
-
-
 
