6c38a496e3150f4e5815b041a28e1801da682d38
Pramika6704062617075 <s6704062617075@email.kmutnb.ac.th>
2026-03-08 15:19:45 +0700

    Commit 4

--- Lab8no2.cpp
+++ Lab8no2.cpp
@@ -3,9 +3,9 @@ int checkscore(char std[]);
 
 int main() {
     int i,j;
-    int count = 0;   // 
+    int count = 0;
 	char s[10];
-	
+
     char ans[8][10]={
         {'A','B','A','C','C','D','E','E','A','D'},
         {'D','B','A','B','C','A','E','E','A','D'},
@@ -17,19 +17,36 @@ int main() {
         {'E','B','E','C','C','D','E','E','A','D'}
     };
 
-    char key1 = 'D';   // 
+    char charkeys[10]={'D','B','D','C','C','D','A','E','A','D'};
+    int correct[10]={0};   //
 
    	for ( i = 0 ; i < 8 ; i++ ){
-   		for(j = 0 ; j < 10 ; j++ )
+   		for(j = 0 ; j < 10 ; j++ ){
    			s[j] = ans[i][j];
 
-        if(ans[i][0] == key1)   // 
+            if(ans[i][j] == charkeys[j])   // 
+                correct[j]++;
+        }
+
+        if(ans[i][0] == charkeys[0])   // 
             count++;
 
    		printf("std %d => %d\n", (i+1), checkscore(s));
 	}
 
     printf("Q1 correct is %d \n", count);
+
+    int min = correct[0];
+    int q = 0;
+
+    for(i=1;i<10;i++){
+        if(correct[i] < min){
+            min = correct[i];
+            q = i;
+        }
+    }
+
+    printf("Hardest is Question %d (number of correct %d)\n", q+1, min);
 }
 
 int checkscore(char std[]){
@@ -42,4 +59,3 @@ int checkscore(char std[]){
 	}
 	return n ;
 }
-
